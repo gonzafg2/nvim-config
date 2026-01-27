b5738b1271be38eaa628615c4de88571ebcaa232
Gonzalo Fleming <gonzafg2@gmail.com>
2026-01-27 13:09:49 -0300

    fix: update leap.nvim repo URL to Codeberg
    Repository moved from ggandor/leap.nvim to codeberg.org/andyg/leap.nvim

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,7 +1,7 @@
 -- Override LazyVim's leap config to fix deprecation warning
 -- Using Sneak-style mappings (see :help leap-mappings)
 return {
-  "ggandor/leap.nvim",
+  url = "https://codeberg.org/andyg/leap.nvim",
   keys = {
     { "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
     { "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
@@ -12,6 +12,16 @@ return {
     for k, v in pairs(opts) do
       leap.opts[k] = v
     end
+
+    -- Opciones para mejor experiencia visual
+    leap.opts.highlight_unlabeled_phase_one_targets = true  -- Resaltar coincidencias inmediatamente
+    leap.opts.safe_labels = {}  -- Forzar mostrar etiquetas siempre (no auto-jump)
+
+    -- Colores más visibles para las etiquetas
+    vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })  -- Atenuar fondo
+    vim.api.nvim_set_hl(0, "LeapMatch", { fg = "#ffffff", bg = "#ff007c", bold = true })
+    vim.api.nvim_set_hl(0, "LeapLabel", { fg = "#000000", bg = "#00ff87", bold = true })
+
     -- Sneak-style mappings (no deprecated functions)
     vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
     vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
