6dc5a1670346c885ddaf4bcf6b44723ba5150587
Gonzalo Fleming <gonzafg2@gmail.com>
2026-01-27 13:18:09 -0300

    fix: simplify eslint config to only start with local installation
    Removed complex handlers and autocmd that caused path errors.
    Now relies on LazyVim extra config, only overriding root_dir to
    return nil when node_modules/eslint doesn't exist.

--- lua/plugins/eslint.lua
+++ lua/plugins/eslint.lua
@@ -1,87 +1,25 @@
+-- Solo sobreescribir root_dir para que ESLint no inicie sin instalación local
+-- LazyVim extra (lazyvim.plugins.extras.linting.eslint) maneja el resto
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      local util = require("lspconfig.util")
-      
-      opts.servers = opts.servers or {}
-      
-      -- Configuración de ESLint LSP
-      opts.servers.eslint = {
-        root_dir = function(fname)
-          -- Buscar proyecto con node_modules o git
-          return util.find_node_modules_ancestor(fname)
-            or util.find_git_ancestor(fname)
-            or util.path.dirname(fname)
-        end,
-        
-        -- Configurar handlers personalizados
-        handlers = {
-          -- Suprimir el mensaje de error molesto
-          ["window/showMessageRequest"] = function(err, result, ctx, config)
-            if result and result.message and result.message:match("ESLint library") then
-              -- Retornar silenciosamente sin mostrar el mensaje
-              return vim.NIL
+    opts = {
+      servers = {
+        eslint = {
+          root_dir = function(filename)
+            local util = require("lspconfig.util")
+            local node_root = util.find_node_modules_ancestor(filename)
+            if node_root then
+              local eslint_path = node_root .. "/node_modules/eslint"
+              if vim.fn.isdirectory(eslint_path) == 1 then
+                return node_root
+              end
             end
-            -- Para otros mensajes, usar el handler por defecto
-            return vim.lsp.handlers["window/showMessageRequest"](err, result, ctx, config)
-          end,
-          
-          -- También suprimir en logs
-          ["window/logMessage"] = function(err, result, ctx, config)
-            if result and result.message and result.message:match("ESLint library") then
-              return vim.NIL
-            end
-            return vim.lsp.handlers["window/logMessage"](err, result, ctx, config)
+            -- nil = no iniciar el servidor
+            return nil
           end,
         },
-        
-        -- Configurar para proyectos sin ESLint local
-        on_new_config = function(config, root_dir)
-          -- Buscar ESLint global si no hay local
-          local local_eslint = util.path.join(root_dir, "node_modules", ".bin", "eslint")
-          if vim.fn.executable(local_eslint) ~= 1 then
-            -- Configurar para usar ESLint global
-            config.cmd = { "eslint", "--stdin" }
-          end
-        end,
-        
-        settings = {
-          packageManager = "npm",
-          useESLintClass = false,
-          experimental = {
-            useFlatConfig = false,
-          },
-          codeAction = {
-            disableRuleComment = {
-              enable = true,
-              location = "separateLine",
-            },
-            showDocumentation = {
-              enable = true,
-            },
-          },
-          codeActionOnSave = {
-            enable = false,
-            mode = "all",
-          },
-          format = false,
-          nodePath = "",
-          onIgnoredFiles = "off",
-          problems = {
-            shortenToSingleLine = false,
-          },
-          quiet = false,
-          rulesCustomizations = {},
-          run = "onType",
-          validate = "on",
-          workingDirectory = {
-            mode = "auto",
-          },
-        },
-      }
-      
-      return opts
-    end,
+      },
+    },
   },
 }
